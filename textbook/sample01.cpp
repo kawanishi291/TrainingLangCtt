c27a2839f84be554bd655aaba4e657efd5974c0a
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-02 01:50:29 +0900

    練習問題1.2_1 関数呼び出し

---
+++ textbook/sample01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void hello_world(void);
+
+int main(){
+
+    hello_world();
+
+    return 0;
+}
+
+void hello_world(){
+
+    std::cout << "hello_world!\nThis is C++" << std::endl;
+
+}
