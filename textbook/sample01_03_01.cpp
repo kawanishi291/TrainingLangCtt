94f7eb74131b903c0052ed485c2ab72e156c5b42
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-02 22:32:53 +0900

    練習問題1.3_1 算術演算子

---
+++ textbook/sample01_03_01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main(){
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    std::cout << "1+2+3=" << a+b+c << std::endl;
+    std::cout << "1+2/3=" << a+b/c << std::endl;
+
+    return 0;
+}
