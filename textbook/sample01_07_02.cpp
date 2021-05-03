28fcbb56e94143fba9d2f47bd941e29023885201
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-03 20:00:10 +0900

    練習問題1.7(2) for文

---
+++ textbook/sample01_07_02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int array[] = {4, 2, 1, 9, 5};
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        std::cout << array[i] << std::endl;
+    }
+
+    return 0;
+}
