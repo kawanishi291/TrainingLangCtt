cc2fae89e46cf563618921e13e8ee310d86274e7
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-03 19:59:56 +0900

    練習問題1.7(1) while文

---
+++ textbook/sample01_07_01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    int array[] = {4, 2, 1, 9, 5};
+    int i = 0;
+
+    while (i < 5) {
+        std::cout << array[i] << std::endl;
+        i += 1;
+    }
+
+    return 0;
+}
