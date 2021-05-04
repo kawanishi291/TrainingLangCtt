2463a6cc59ae5169edd13f0ae4f07776e7e3d0ec
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-05 03:01:13 +0900

    1並び数字の整形出力

---
+++ explain/ex3_002.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int i;
+    int sum = 0;
+
+    for (i = 0; i < 6; i++) {
+        sum *= 10;
+        sum += 1;
+        std::cout << sum << std::endl;
+    }
+
+    return 0;
+}
