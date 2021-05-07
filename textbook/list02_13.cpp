c41953bb142287ccf7e3b91fce49fa47af5e91a0
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-07 15:24:14 +0900

    ネストした型名

--- textbook/list02_13.cpp
+++ textbook/list02_13.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
+/* ネストした型名 */
+
 class data
 {
     public:
-        using integer = int; // クラスの中で使う整数に別名を与える
+        using integer = int;    // クラスの中で使う整数に別名を与える
+
+/* クラスの中では別名を直接使える */
         integer get_value();
         void set_value(integer new_value);
 
@@ -11,12 +15,14 @@ class data
         integer value;
 };
 
+/* クラスの外ではスコープ解決演算子を使う */
 data::integer data::get_value()
 {
     return value;
 }
 
-
+/* 　メンバー関数の中ではクラスの内側という扱いなので、
+    スコープ解決演算子を使わなくてもネストした型名を使える  */
 void data::set_value(integer new_value)
 {
     integer tmp = new_value;
@@ -27,11 +33,10 @@ void data::set_value(integer new_value)
 int main(){
     data d;
 
+/* ネストした型名は長くなりがちなので、必要に応じてさらに別名を与えるとよい */
     using integer = data::integer;
-    integer value = 42;
-
+    integer value = 42;     // inntegerは data::integer の別名
     d.set_value(value);
-
     std::cout << d.get_value() << std::endl;
 
     return 0;
