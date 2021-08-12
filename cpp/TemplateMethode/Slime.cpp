1966c796450aec7ecbe48b7e1af58122f55ebfb3
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-08-12 22:22:37 +0900

    派生クラスC++ファイル

--- cpp/TemplateMethode/Slime.cpp
+++ cpp/TemplateMethode/Slime.cpp
@@ -3,3 +3,10 @@
 Slime::Slime() : name("Slime"), atk(10), def(10) {
     std::cout << name << std::endl;
 }
+
+std::string Slime::getName() {
+
+    std::cout << __func__ << "()" << std::endl;
+
+    return name;
+}
