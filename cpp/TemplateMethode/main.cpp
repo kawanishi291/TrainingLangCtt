595eea28afd96efebacaef3aa034bbdbb1ff0208
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-08-12 22:19:15 +0900

    main()ファイル

--- cpp/TemplateMethode/main.cpp
+++ cpp/TemplateMethode/main.cpp
@@ -1,10 +1,13 @@
 #include "./Slime.hpp"
+#include "./Dragon.hpp"
 
 
 int main() {
     Slime slime;
+    Dragon dragon;
 
     std::cout << slime.getName() << std::endl;
+    std::cout << dragon.getName() << std::endl;
 
     return 0;
 }
